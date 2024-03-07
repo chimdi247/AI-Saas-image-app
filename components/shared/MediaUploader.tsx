b900a27e8f448568ea6ec6e10273bc376e3f5085
chimdi247 <Chimdi247@gmail.com>
2024-03-08 00:41:13 +0200

    added upload preset

--- components/shared/MediaUploader.tsx
+++ components/shared/MediaUploader.tsx
@@ -53,7 +53,7 @@ const MediaUploader = ({
 
   return (
     <CldUploadWidget
-      uploadPreset="jsm_imaginify"
+      uploadPreset="hq71xcjv"
       options={{
         multiple: false,
         resourceType: "image",
